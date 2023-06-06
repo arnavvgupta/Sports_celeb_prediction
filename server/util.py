e4c1287be5c3f068f0de8d8b632639225456961d
Arnav Gupta <arnavgupta@Arnavs-MacBook-Air.local>
2023-06-06 11:10:12 +0530

    updated util.py

--- server/util.py
+++ server/util.py
@@ -40,13 +40,13 @@ def load_saved_artifacts():
     global __class_name_to_number
     global __class_number_to_name
 
-    with open("server/artifacts/class_dictionary.json", "r") as f:
+    with open("/artifacts/class_dictionary.json", "r") as f:
         __class_name_to_number = json.load(f)
         __class_number_to_name = {v:k for k,v in __class_name_to_number.items()}
 
     global __model
     if __model is None:
-        with open('server/artifacts/saved_model.pkl', 'rb') as f:
+        with open('/artifacts/saved_model.pkl', 'rb') as f:
             __model = joblib.load(f)
     print("loading saved artifacts...done")
 
